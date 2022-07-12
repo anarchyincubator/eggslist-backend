d233dc356db637b4fa23f7c549db7a2086956573
Fedor Grab <fvgrab@gmail.com>
2022-07-12 14:32:26 -0400

    Feat: Add location search

--- eggslist/site_configuration/api/views.py
+++ eggslist/site_configuration/api/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 
 from eggslist.site_configuration import filters, models
 from eggslist.site_configuration.api import serializers
@@ -16,7 +17,8 @@ class LocationCityListAPIView(CacheListAPIMixin, generics.ListAPIView):
     cache_key = "location_cities"
     serializer_class = serializers.CityLocationSerializer
     queryset = models.LocationCity.objects.select_related("state__country").all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    search_fields = ("name", "state__name", "state__country__name")
     filterset_class = filters.LocationCityFilter
 
 
