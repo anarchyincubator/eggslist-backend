6333ae874937449b77048e76f2cae075e99f492a
Fedor Grab <fvgrab@gmail.com>
2023-06-30 15:50:33 -0400

    Chore: Update num_workers to 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn wsgi:application --workers 2 --timeout 600 --bind 0.0.0.0:80
+gunicorn wsgi:application --workers 1 --timeout 600 --bind 0.0.0.0:80
 
