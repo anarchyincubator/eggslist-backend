2cc32ddab1332b98e75153bc385373c78fb46200
fedorgrab <fvgrab@gmail.com>
2023-08-10 15:43:58 -0400

    Chore: production settings Debug -> False

--- app/settings/production.py
+++ app/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *  # noqa
 
-DEBUG = True
+DEBUG = False
 SITE_URL = "https://eggslist.com"
 SESSION_COOKIE_DOMAIN = "backend.eggslist.com"
 CSRF_TRUSTED_ORIGINS = ["https://backend.eggslist.com", "https://eggslist.com"]
